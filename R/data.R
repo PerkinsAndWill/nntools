c6eec79afa162d4d04df5a2f648fa7f5a045b360
Bryan Blanc <bpb824@users.noreply.github.com>
2022-02-01 19:47:01 -0800

    added base color palette

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Base NN Color Palette (2022)
+#'
+#' Definition (hex codes and categories) for 2022 NN colors
+#'
+#' @format A data frame with 18 rows and 3 variables
+#' \describe{
+#'   \item{color_name}{NN Name of Color}
+#'   \item{hex_code}{Hexadecimal code definition of color}
+#'   \item{color_category}{Recommended category of color (e.g., primary, secondary)}
+#' }
+#' @source Brendan Rahman, February 1st, 2022
+"nn_base_color_palette"
---
+++ data-raw/nn_base_color_palette.R
@@ -0,0 +1,26 @@
+## code to prepare `nn_base_color_palette` dataset goes here
+library(tibble)
+
+nn_base_color_palette = tribble(
+  ~color_name, ~hex_code, ~color_category,
+  'NN Blue','#006d9d', 'Primary',
+  'NN Teal',	'#00a4b9',	'Accent',
+  'NN Gold',	'#fbab18',	'Accent',
+  'NN Red',	'#d63f3e',	'Accent',
+  'NN Mauve',	'#d74341',	'Accent',
+  'NN Midnight',	'#002934',	'Background',
+  'NN Harbor',	'#ebebeb',	'Background',
+  'NN Black',	'#000000',	'Background',
+  'NN White',	'#FFFFFF',	'Background',
+  'NN Sky',	'#d74341',	'Supplemental',
+  'NN Green',	'#4ea652',	'Supplemental',
+  'NN Pear',	'#ccc72c',	'Supplemental',
+  'NN Lemon',	'#ffcb05',	'Supplemental',
+  'NN Carrot',	'#f47d20',	'Supplemental',
+  'NN Magenta',	'#cc3d72',	'Supplemental',
+  'NN Pink',	'#f597b2',	'Supplemental',
+  'NN Purple',	'#915ea6',	'Supplemental',
+  'NN Chestnut',	'#644a49',	'Supplemental'
+)
+
+usethis::use_data(nn_base_color_palette, overwrite = TRUE)
